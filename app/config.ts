15d04d167a6d64d503025f2f0ad5119bfc9bb4a1
Corey Owens <152214419+coreyswordsweeper@users.noreply.github.com>
2024-11-09 13:04:28 -0600

    update title

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://coreyowens.me",
+  title: "Corey Owens",
+  name: "Corey Owens",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/owenscorey",
+  github: "https://github.com/coreyinfinitecreations",
+  instagram: "https://www.instagram.com/owenscorey",
+  linkedin: "https://www.linkedin.com/in/coreymowens",
+  email: "mailto:corey@coreyowens.com",
 };
